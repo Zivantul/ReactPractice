1773617158c6561def6c6f28be2f5c92142dff4b
[zivan] <ivanzuev@Pronka-new.local>
2022-09-11 12:56:15 +0300

    Add DZ 7

--- message_app/src/components/message-list/message-list.js
+++ message_app/src/components/message-list/message-list.js
@@ -9,11 +9,11 @@ import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 // import PropTypes from "prop-types";
 import { InputAdornment } from "@mui/material";
-import { sendMessage, messagessSelector } from "../../store/messages";
+import { sendMessageWithBot, messagessSelector } from "../../store/messages";
 import { Message } from "./message";
 import { Input, SendIcon } from "./styles";
 
-// @TODO  переделать как в https://codesandbox.io/s/gbchat-router-7fg2fn?file=/src/App.js:1887-1898
+
 export const MessageList = () => {
     const { roomId } = useParams();
 
@@ -30,7 +30,7 @@ export const MessageList = () => {
     const send = useCallback(
         (message, author = "User") => {
             if (message) {
-                dispatch(sendMessage(roomId, { message, author }));
+                dispatch(sendMessageWithBot(roomId, { message, author }));
                 setValue("");
             }
         },
@@ -85,22 +85,10 @@ export const MessageList = () => {
                 onKeyPress={handlePressInput}
                 endAdornment={
                     <InputAdornment position="end">
-                        {value && <SendIcon onClick={send} />}
+                        {value && <SendIcon onClick={() => send(value)} />}
                     </InputAdornment>
                 }
             />
         </>
     );
 };
-
-  // MessageList.propTypes = {
-  //   message: PropTypes.string.isRequired,
-  //   o1: PropTypes.shape({
-  //     s1: PropTypes.string.isRequired,
-  //   }).isRequired,
-  //   a: PropTypes.arrayOf(
-  //     PropTypes.shape({
-  //       s1: PropTypes.string.isRequired,
-  //     }).isRequired
-  //   ).isRequired,
-  // };
